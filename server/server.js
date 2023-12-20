28a535e0b1efa1d294f5e2b4e57ee7b1e49e9f09
mpxfactor <tancmpsinc@gmail.com>
2023-12-20 08:31:45 +0530

    few more htmx usage

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bodyParse from "body-parser";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -9,14 +9,31 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
-app.use(bodyParse.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-    res.send("<h1>Welcome to HTMX GET Request</h1>")
-})
+  res.send("<h1>Welcome to HTMX GET Request</h1>");
+});
 
 app.post("/", (req, res, next) => {
   res.send("<h1>Welcome to HTMX POST Request</h1>");
+});
+
+app.post("/post", (req, res, next) => {
+  setTimeout(() => res.send("<h1>Welcome to HTMX POST Request</h1>"), 5000);
+});
+
+app.post("/echopayload", (req, res, next) => {
+  const { email, password } = req.body;
+
+  // return res.send(`<div><h1>Email = ${email}, Password = ${password}</h1></div>`);
+
+  setTimeout(
+    () =>
+      res.send(`<div><h1>Email = ${email}, Password = ${password}</h1></div>`),
+    2000
+  );
 });
 
 app.listen(PORT, () => {
